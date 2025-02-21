b85b2e27be09d3f92c17b8513e256942ea5e4d9a
TwinklerG <231830131@smail.nju.edu.cn>
2025-02-21 23:18:49 +0800

    add more basic components

--- components/messages.tsx
+++ components/messages.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
 export function Messages() {
+  const params = useParams<{ id: string }>();
+
   return (
     <div className="inline-block w-full h-full p-5">
-      <div className="w-full h-[70%] rounded-lg border-2 flex items-center justify-center">
-        <div className="text-2xl">Welcome to finerseek!</div>
+      <div className="w-full h-[70%] rounded-lg border-2 flex items-center justify-center animate-scaleIn">
+        <div className="text-2xl">
+          {params.id === undefined
+            ? "Welcome to finerseek!"
+            : `You are in history record ${params.id}`}
+        </div>
       </div>
       <div className="w-full h-[30%] flex items-center justify-center p-2">
         <div className="rounded-lg border-2 w-full h-full text-center">
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,14 +1,77 @@
 import clsx from "clsx";
+import { redirect, useParams } from "next/navigation";
 
 export function Sidebar({ hide }: { hide: boolean }) {
+  const params = useParams<{ id: string }>();
+  const id = Number(params.id);
+
+  const historys = [
+    "第1条历史记录",
+    "第2条历史记录",
+    "第3条历史记录",
+    "第4条历史记录",
+    "第1条历史记录",
+    "第2条历史记录",
+    "第3条历史记录",
+    "第4条历史记录",
+    "第1条历史记录",
+    "第2条历史记录",
+    "第3条历史记录",
+    "第4条历史记录",
+    "第1条历史记录",
+    "第2条历史记录",
+    "第3条历史记录",
+    "第4条历史记录",
+    "第1条历史记录",
+    "第2条历史记录",
+    "第3条历史记录",
+    "第4条历史记录",
+    "第1条历史记录",
+    "第2条历史记录",
+    "第3条历史记录",
+    "第4条历史记录",
+    "第1条历史记录",
+    "第2条历史记录",
+    "第3条历史记录",
+    "第4条历史记录",
+  ];
   return (
     <div
       className={clsx(
-        "bg-slate-200 dark:bg-gray-800 h-full transition-all ease-in-out duration-300",
+        "bg-slate-200 dark:bg-gray-800 max-h-full transition-all ease-in-out duration-300 flex flex-col overflow-hidden",
         hide ? "w-0" : "w-[30vw]"
       )}
     >
-      <div className={clsx(hide ? "hidden" : "")}>This is Sidebar</div>
+      <div className="w-full p-4">
+        <div className="bg-gray-600 rounded-md p-4 hover:cursor-pointer" onClick={() => {
+          redirect("/app/chatbot")
+        }}>
+          开启新的对话
+        </div>
+      </div>
+      <ul className="overflow-y-scroll">
+        {historys.map((history, index) =>
+          id === index ? (
+            <li
+              key={index}
+              className=" bg-gray-600 rounded-md hover:cursor-pointer  h-10 p-2 m-2"
+              onClick={() => {
+                redirect(`/app/chatbot/${index}`);
+              }}
+            ></li>
+          ) : (
+            <li
+              key={index}
+              className=" bg-gray-500 rounded-md hover:cursor-pointer hover:bg-gray-600 h-10 p-2 m-2"
+              onClick={() => {
+                redirect(`/app/chatbot/${index}`);
+              }}
+            >
+              {history}
+            </li>
+          )
+        )}
+      </ul>
     </div>
   );
 }
